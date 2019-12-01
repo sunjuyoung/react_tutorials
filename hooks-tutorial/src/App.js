6418224c83a2c24b284288bb84254d76fcd0df71
syseoz <syseoz@naver.com>
2019-12-01 22:52:00 +0900

    react

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,16 +1,28 @@
 import React ,{useState}from 'react';
 import Counter from './Counter';
 import Info from './Info';
+import Average from './Average';
 
 
 const App = ()=> {
+
   const [visible, setVisible] = useState('');
+
+
+
   return (
+
     <div>
-<button onClick={()=>setVisible(!visible)}>{visible ? '숨기긱':'보이기'}</button>
-<hr></hr>
-        {visible &&<Info />}
+
+      {visible ? <Average /> : null}
+      <hr></hr>
+      <button onClick={()=>setVisible(!visible)}>{visible ? '보이기' : '숨기기'}</button>
     </div>
+
+
+
+
+
   );
 }
 
---
+++ hooks-tutorial/src/Average.js
@@ -0,0 +1,47 @@
+import React ,{useState} from 'react';
+
+const getAverage = number =>{
+    console.log('평균값 계산');
+    if(number.length === 0) return 0;
+    
+    const sum = number.reduce((a,b)=>a+b);
+    return sum / number.length;
+};
+
+
+
+const Average = () => {
+
+    const [list,setList] = useState([]);
+    const [number,setNumber] = useState('');
+
+    const onChange= e =>{
+        setNumber(e.target.value);
+    }
+
+    const onInsert = e =>{
+        const nextList = list.concat(parseInt(number));
+        setList(nextList);
+        setNumber('');
+    }
+
+    return (
+        <div>
+            <input value={number} onChange={onChange}></input>
+            <button onClick={onInsert}>등록</button>
+            <ul>
+                {list.map((value,index)=>(
+                    <li key={index}>{value}</li>
+    ))}
+            </ul>
+            <div>
+                <b>평균값 : {getAverage(list)}</b>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Average;
+
